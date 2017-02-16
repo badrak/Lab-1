01a30654183be63cba6cba3a4f4440f7770a5353
badrak <andrewbadrak29@gmail.com>
2017-02-16 17:46:55 +0200

    Create iterator.py

---
+++ iterator.py
@@ -0,0 +1,13 @@
+def my_for(iterable, callback):
+    it = iter(iterable)
+    while True:
+        try:
+            value = next(it)
+            callback(value)
+        except StopIteration:
+            break
+
+def loop_body(value):
+    print('Next value receied: ', value)
+
+my_for(range(10), loop_body)
